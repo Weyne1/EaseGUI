cf103685d2c7c961397466a9af0381a3394f581c
Weyne1 <weyne931@gmail.com>
2026-07-05 17:09:13 +0300

    ref: TitleScreenMixin

--- common/src/main/java/net/weyne1/easegui/client/mixin/gui/title/TitleScreenMixin.java
+++ common/src/main/java/net/weyne1/easegui/client/mixin/gui/title/TitleScreenMixin.java
@@ -1,28 +1,34 @@
 package net.weyne1.easegui.client.mixin.gui.title;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.weyne1.easegui.client.config.ConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 
     /**
-     * Cancels the vanilla TitleScreen setWidgetAlpha animation if EaseGUI is enabled for this screen.
+     * Cancels the vanilla TitleScreen fadeWidgets animation if EaseGUI is enabled for this screen.
      * This allows EaseGUI to fully control widget fade-in animation,
      * while preserving vanilla transitions if the screen animation is disabled.
      */
-    @Inject(method = "setWidgetAlpha", at = @At("HEAD"), cancellable = true)
-    private void easeGUI$cancelFade(float alpha, CallbackInfo ci) {
+    @WrapOperation(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screens/TitleScreen;fadeWidgets(F)V"
+            )
+    )
+    private void easeGUI$conditionallyFadeWidgets(TitleScreen instance, float v, Operation<Void> original) {
         var titleSettings = ConfigManager.getConfig().screens.get("title");
 
-        if (titleSettings == null || !titleSettings.enabled) {
+        if (titleSettings != null && titleSettings.enabled) {
             return;
         }
 
-        ci.cancel();
+        original.call(instance, v);
     }
 }
