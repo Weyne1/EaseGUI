cc4f7c81752aa63e0fd9432435111646ac46fb83
Weyne1 <weyne931@gmail.com>
2026-07-08 17:37:39 +0300

    ref: extract AnimationState into a top-level class

--- common/src/main/java/net/weyne1/easegui/client/animation/AnimationState.java
+++ common/src/main/java/net/weyne1/easegui/client/animation/AnimationState.java
@@ -1,11 +1,8 @@
 package net.weyne1.easegui.client.animation;
 
-public class AnimationState {
-
-    public static final class AnimationData {
-        public long startTime = -1;
-        public long delay = 0;
-        public boolean init = false;
-        public int lastRenderFrame = -1;
-    }
+public final class AnimationState {
+    public long startTime = -1;
+    public long delay = 0;
+    public boolean init = false;
+    public int lastRenderFrame = -1;
 }
