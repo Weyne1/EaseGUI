6e2e47b5991dd042940e3d252fb94b8ef36cd2a6
Weyne1 <weyne931@gmail.com>
2026-07-14 15:12:08 +0300

    ref: oops the fabric package was named neoforge

--- fabric/src/main/java/net/weyne1/easegui/fabric/client/ModMenuIntegration.java
+++ fabric/src/main/java/net/weyne1/easegui/fabric/client/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package net.weyne1.easegui.neoforge.client;
+package net.weyne1.easegui.fabric.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
