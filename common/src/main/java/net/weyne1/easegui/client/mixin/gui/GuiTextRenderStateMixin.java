768927d66d570bec12d83dd8c3e732492e811fda
Weyne1 <weyne931@gmail.com>
2026-07-25 01:39:33 +0300

    fix: animate the backgroundColor for the text

--- common/src/main/java/net/weyne1/easegui/client/mixin/gui/GuiTextRenderStateMixin.java
+++ common/src/main/java/net/weyne1/easegui/client/mixin/gui/GuiTextRenderStateMixin.java
@@ -7,13 +7,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@SuppressWarnings("ModifyVariableMayUseName")
 @Mixin(GuiTextRenderState.class)
 public class GuiTextRenderStateMixin {
 
-    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true, ordinal = 2)
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true, name = "color")
     private static int easeGUI$modifyTextStateColor(int color) {
         if (!AnimationContext.isActive()) return color;
         return ColorUtils.getAnimatedColor(color);
+    }
+
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true, name = "backgroundColor")
+    private static int easeGUI$modifyTextStateBackgroundColor(int backgroundColor) {
+        if (!AnimationContext.isActive() || backgroundColor == 0) return backgroundColor;
+        return ColorUtils.getAnimatedColor(backgroundColor);
     }
 }
