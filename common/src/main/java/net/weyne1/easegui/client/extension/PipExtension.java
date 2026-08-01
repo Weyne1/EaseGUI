da7ae1861e206fb6c7a22c26f1d0412b067f4b60
Weyne1 <weyne931@gmail.com>
2026-08-01 15:57:57 +0300

    feat: add alpha animation for PIP elements

---
+++ common/src/main/java/net/weyne1/easegui/client/extension/PipExtension.java
@@ -0,0 +1,5 @@
+package net.weyne1.easegui.client.extension;
+
+public interface PipExtension {
+    float easegui$getAlpha();
+}
---
+++ common/src/main/java/net/weyne1/easegui/client/mixin/gui/pip/PipRenderStateMixin.java
@@ -0,0 +1,34 @@
+package net.weyne1.easegui.client.mixin.gui.pip;
+
+import net.minecraft.client.gui.render.state.pip.*;
+import net.weyne1.easegui.client.animation.AnimationContext;
+import net.weyne1.easegui.client.extension.PipExtension;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin({
+        GuiSkinRenderState.class,
+        GuiEntityRenderState.class,
+        GuiBookModelRenderState.class,
+        GuiBannerResultRenderState.class,
+        OversizedItemRenderState.class
+})
+public abstract class PipRenderStateMixin implements PipExtension {
+
+    @Unique private float easegui$alpha = 1.0f;
+
+    @Inject(method = "<init>*", at = @At("RETURN"))
+    private void easeGUI$captureAlpha(CallbackInfo ci) {
+        if (AnimationContext.isActive()) {
+            this.easegui$alpha = AnimationContext.getCurrentAlpha();
+        }
+    }
+
+    @Override
+    public float easegui$getAlpha() {
+        return this.easegui$alpha;
+    }
+}
