445f301a63d868677e15ca2a744bd925d12207ee
Weyne1 <weyne931@gmail.com>
2026-08-01 16:05:08 +0300

    Remove unnecessary hover control from widget

--- common/src/main/java/net/weyne1/easegui/client/mixin/gui/components/AbstractWidgetMixin.java
+++ common/src/main/java/net/weyne1/easegui/client/mixin/gui/components/AbstractWidgetMixin.java
@@ -2,24 +2,18 @@ package net.weyne1.easegui.client.mixin.gui.components;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.util.Util;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.*;
 import net.weyne1.easegui.client.extension.WidgetExtension;
 import net.weyne1.easegui.client.animation.AnimationScope;
 import net.weyne1.easegui.client.animation.AnimationState;
 import net.weyne1.easegui.client.animator.WidgetAnimator;
-import net.weyne1.easegui.client.config.ConfigManager;
 import net.weyne1.easegui.api.WidgetCategory;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(AbstractWidget.class)
 public abstract class AbstractWidgetMixin implements WidgetExtension {
-
-    @Shadow protected float alpha;
-    @Shadow protected boolean isHovered;
 
     @Unique private final AnimationState easeGUI$animationState = new AnimationState();
     @Unique private WidgetCategory easeGUI$cachedCategory = null;
@@ -48,27 +42,7 @@ public abstract class AbstractWidgetMixin implements WidgetExtension {
         }
 
         try (AnimationScope ignored = WidgetAnimator.beginRender(widget, guiGraphics, category, this.easeGUI$animationState)) {
-
-            var profile = ConfigManager.getProfileForCurrentContext(category);
-            boolean shouldBypassHover = false;
-
-            if (profile != null && profile.isEnabled() && this.easeGUI$animationState.init) {
-                long elapsed = Util.getMillis() - this.easeGUI$animationState.startTime - this.easeGUI$animationState.delay;
-                if (elapsed < profile.getDuration()) {
-                    shouldBypassHover = true;
-                }
-            }
-
-            if (shouldBypassHover) {
-                boolean savedHover = this.isHovered;
-                this.isHovered = false;
-
-                original.call(guiGraphics, mouseX, mouseY, partialTick);
-
-                this.isHovered = savedHover;
-            } else {
-                original.call(guiGraphics, mouseX, mouseY, partialTick);
-            }
+            original.call(guiGraphics, mouseX, mouseY, partialTick);
         }
     }
 }
