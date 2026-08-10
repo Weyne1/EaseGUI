f5954d908681246895c586b176adcba4f38aff19
Weyne1 <weyne931@gmail.com>
2026-08-10 19:57:37 +0300

    ref: rename AnimationState to WidgetAnimationState

--- common/src/main/java/net/weyne1/easegui/client/animation/WidgetAnimationState.java
+++ common/src/main/java/net/weyne1/easegui/client/animation/WidgetAnimationState.java
@@ -1,6 +1,6 @@
 package net.weyne1.easegui.client.animation;
 
-public final class AnimationState {
+public final class WidgetAnimationState {
     public long startTime = -1;
     public long delay = 0;
     public boolean init = false;
